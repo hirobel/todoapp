5d9d665c82e8357e5b6a5605aa5a5948a9c78fbf
Hiroaki Kato <>
2019-02-15 12:06:30 +0900

    現状コミット

---
+++ 4_src/3_other/5_try-serverless-cognito/cognito-tutorial/src/libs/api/api.js
@@ -0,0 +1,13 @@
+import { Auth, API } from 'aws-amplify'
+
+const API_NAME = 'cognito-tutorial-api'
+
+export async function getTest() {
+  const user = await Auth.currentAuthenticatedUser()
+  const token = user.signInUserSession.idToken.jwtToken
+  return API.get(API_NAME, '/dev/hello', {
+    headers: {
+      Authorization: token
+    }
+  })
+}
